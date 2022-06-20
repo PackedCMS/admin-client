cc202d9401aa2a4eed9d866052d915b805b11afb
Raksix <ermagfurkan7@gmail.com>
2022-06-20 15:01:53 +0300

    update

---
+++ src/components/panels/endpoints/index.js
@@ -0,0 +1,15 @@
+import AdminPanel from "../panel/panel"
+
+
+export default function EndpointsPanels() {
+
+   return (
+      <div>
+         <div className="admin-panels-one">
+            <AdminPanel head="Endpointler">
+               
+            </AdminPanel>
+         </div>
+      </div>
+   )
+}
